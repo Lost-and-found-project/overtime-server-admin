30a442a84bc76b3040d3f5102964d6d3f61ed359
KurumiW <2209278137@qq.com>
2021-10-27 15:47:15 +0800

    optimize

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -4,13 +4,13 @@ import {
   removeStorage,
   setSession,
   getSession,
-  removeSession
-} from "./storage.js"
+  removeSession,
+} from "./storage.js";
 
-import * as C from "./constants.js"
+import * as C from "./constants.js";
 
-export const setTabList = (p) => setSession(C.ADMIN_TABLIST, p)
-export const getTabList = () => getSession(C.ADMIN_TABLIST, true)
+export const setTabList = (p) => setSession(C.ADMIN_TABLIST, p);
+export const getTabList = () => getSession(C.ADMIN_TABLIST, true);
 
-export const setTabPath = p => setSession(C.ADMIN_TABPATH, p)
-export const getTabPath = () => getSession(C.ADMIN_TABPATH, true)
+export const setTabPath = (p) => setSession(C.ADMIN_TABPATH, p);
+export const getTabPath = () => getSession(C.ADMIN_TABPATH, true);
--- src/views/home/components/main/index.vue
+++ src/views/home/components/main/index.vue
@@ -24,7 +24,7 @@ import {
   getTabList,
   setTabPath,
   getTabPath,
-} from "../../../../utils/storage/index.js"
+} from "../../../../utils/storage/index.js";
 export default {
   data() {
     return {
@@ -35,86 +35,84 @@ export default {
           path: "/index",
         },
       ],
-    }
+    };
   },
   methods: {
     handleMiddleClick(e) {
-      const { path } = this.routeList.find((item) => {
-        return e.target.innerText === item.title
-      })
-      this.handleTabRemove(path)
+      const { path } = this.routeList.find(
+        (item) => e.target.innerText === item.title
+      );
+      this.handleTabRemove(path);
     },
     // 查重
     findObject(path) {
-      const flag = this.routeList.find((item) => {
-        return item.path === path
-      })
-      return !!flag
+      const flag = this.routeList.find((item) => item.path === path);
+      return !!flag;
     },
     handleTabClick(tab) {
-      this.$router.replace({ path: tab.props.name })
+      this.$router.replace({ path: tab.props.name });
     },
     handleTabRemove(path) {
-      let idx = null
+      let idx = null;
       this.routeList = this.routeList.filter((item, index) => {
         if (item.path === path) {
-          idx = index
+          idx = index;
         }
-        return item.path !== path
-      })
+        return item.path !== path;
+      });
       if (this.routeList.length <= 0) {
         this.routeList = [
           {
             title: "首页",
             path: "/index",
           },
-        ]
-        this.active = "/index"
-        this.$router.replace({ path: "/index" })
+        ];
+        this.active = "/index";
+        this.$router.replace({ path: "/index" });
       }
       if (!this.findObject(this.$route.path)) {
-        const routeIdx = idx === this.routeList.length ? 0 : idx
-        this.$router.replace({ path: this.routeList[routeIdx].path })
-        this.active = this.routeList[routeIdx].path
+        const routeIdx = idx === this.routeList.length ? 0 : idx;
+        this.$router.replace({ path: this.routeList[routeIdx].path });
+        this.active = this.routeList[routeIdx].path;
       }
     },
   },
   created() {
     if (getTabList() && getTabPath()) {
-      this.active = getTabPath()
-      this.routeList = getTabList()
+      this.active = getTabPath();
+      this.routeList = getTabList();
     } else {
       if (this.$route.path !== "/index") {
         this.routeList.push({
           title: this.$route["meta"].title,
           path: this.$route.path,
-        })
-        this.active = this.$route.path
+        });
+        this.active = this.$route.path;
       }
     }
   },
   watch: {
     $route(val) {
-      const { title } = val["meta"]
+      const { title } = val["meta"];
       if (this.findObject(val.path)) {
-        this.active = val.path
+        this.active = val.path;
       } else {
         this.routeList.push({
           title: title,
           path: val.path,
-        })
-        this.active = val.path
+        });
+        this.active = val.path;
       }
-      setTabPath(val.path)
+      setTabPath(val.path);
     },
     routeList: {
       handler(val) {
-        setTabList(val)
+        setTabList(val);
       },
       deep: true,
     },
   },
-}
+};
 </script>
 
 <style></style>
